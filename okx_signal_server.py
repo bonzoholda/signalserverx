840d9202eec72e6cc7aae770be59cce3bda35f5f
bonzoholda <bhumibeka@gmail.com>
2025-06-12 23:18:08 +0700

    Update okx_signal_server.py

--- okx_signal_server.py
+++ okx_signal_server.py
@@ -200,15 +200,15 @@ def signal_loop():
 
                 # Priority logic
                 if bull_div:
-                    sig = 'long-divergence' ##remove -hold to activate, add it back to make inactive
+                    sig = 'long-divergence-hold' ##remove -hold to activate, add it back to make inactive
                 elif bear_div:
-                    sig = 'short-divergence' ##remove -hold to activate, add it back to make inactive
+                    sig = 'short-divergence-hold' ##remove -hold to activate, add it back to make inactive
                 elif ml_signal in ['long', 'short']:
                     sig = f"{ml_signal}-ml"
                 elif ta_signal == 'buy':
-                    sig = 'long'
+                    sig = 'long-hold'
                 elif ta_signal == 'sell':
-                    sig = 'short'
+                    sig = 'short-hold'
                 else:
                     sig = 'no-signals'
 
